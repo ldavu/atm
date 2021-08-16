1c4d025f56629fc4cb400787c475cbe31bc73e10
ldavu <41762352+ldavu@users.noreply.github.com>
2021-08-16 10:35:32 -0300

    Update atm.py

--- atm.py
+++ atm.py
@@ -1,6 +1,6 @@
 print("Bem-Vindo ao Caixa eletrónico online!")
 restart = 'y'
-saldo = (f"R${1500.00}")
+saldo = 1500
 chances = 3
 while chances >= 0:
     senha = int(input("Digite sua senha de 4 dígitos: "))
@@ -18,32 +18,30 @@ while chances >= 0:
                 break
         elif opcao == 2:
             print("Qual o valor que deseja retirar da carteira? 10, 20, 50, 100 para outros valores digite 1")
-            retirar = input("")
-            if retirar in [10,20,50,100]:
-                retirar_atual = retirar - saldo
-                print(f"Seu saldo é de: {saldo}")
+            retirar = float(input())
+            if retirar == 1:
+                print("Quanto deseja retirar?")
+                retirar = float(input())
+                saldo_atual = saldo - retirar
+                print(f"Seu saldo atual é de: {saldo_atual}")
                 continuar = str(input("Deseja continuar?"))
                 if continuar in ('n','N','não','nao'):
                     break
-            elif retirar != [10, 20, 50, 100]:
-                print("Desculpe, coloque um valor válido!")
-            if retirar == 1:
-                print("Quanto deseja retirar?")
-                retirar = float(input(""))
-                if retirar in [10,20,50,100]:
-                    print("Coloque um valor válido!")
-                else:
-                    saldo_atual = retirar - saldo
-                    print(f"Seu saldo atual é de: {saldo}")
-                    continuar = str(input("Deseja continuar?"))
-                    if continuar in ('n','N','não','nao'):
-                        break
+            if retirar in [10, 20, 50, 100]:
+                retirar_atual = saldo - retirar
+                print(f"Seu saldo é de: {retirar_atual}")
+                continuar = str(input("Deseja continuar?"))
+                if continuar in ('n','N','não','nao'):
+                    break
+            elif retirar in [10,20,50,100]:
+                print('Informe um valor válido!')
+
             
         elif opcao == 3:
             print("Qual o valor que deseja depositar?")
             depositar_valor = float(input(""))
-            depostiar_valor_saldo = depositar_valor + saldo
-            print(f"Seu saldo é de: {saldo}")
+            depositar_valor_saldo = saldo + depositar_valor
+            print(f"Seu saldo é de: {depositar_valor_saldo}")
         elif opcao == 4:
             pass
 
